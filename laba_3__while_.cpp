625439d4214c2c92b526ea3eee614b650c45bf08
inkoromi21 <sema.vorobev20078@gmail.com>
2025-09-29 19:09:49 +0700

    laba_3_"while"

---
+++ laba_3__while_.cpp
@@ -0,0 +1,46 @@
+/*************************
+* Автор: Воробьев Семен  *
+* Вариант: 3             *
+*************************/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main () {
+  int R, E;
+  double P, r;
+  
+  cout << "E = ";
+  cin  >> E;
+  
+  cout << "r = ";
+  cin  >> r;
+
+  cout << "R = ";
+  cin  >> R;
+
+  cout << fixed << setprecision(2) << "\t \t" << endl;
+    
+  while(R < 50.0) {
+    P = R * (E / (R + r)) * (E / (R + r));
+
+    cout << "R = " << R << "\t \t" << P << endl;
+      
+    // увеличение сопротивления до 50 Ом.
+    R = R + 10.0 ;
+    
+  }
+
+  do {
+    P = R * (E / (R + r)) * (E / (R + r));
+  
+    cout << "R = " << R << "\t \t" << P << endl;
+      
+    // увеличение сопротивления до 350 Ом.
+    R = R + 50.0;
+  } while(R < 350.0);
+  
+  return 0;
+}
